17a31e184d720a4fdc9eb5a5d71b65b5a39bf813
Alfred <alfred@codenaturally.com>
2018-02-12 22:58:15 -0800

    getting started with matterjs

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,27 @@
-console.log('hello');
+import $ from 'jquery';
+import {
+    Engine,
+    Render,
+    Bodies,
+    World,
+} from 'matter-js';
+
+$(document).ready(() => {
+    console.log(document);
+    const engine = Engine.create();
+    
+    var render = Render.create({
+        element: document.body,
+        engine: engine
+    });
+    
+    var boxA = Bodies.rectangle(400, 200, 80, 80);
+    var boxB = Bodies.rectangle(450, 50, 80, 80);
+    var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+    
+    World.add(engine.world, [boxA, boxB, ground]);
+    
+    Engine.run(engine);
+    
+    Render.run(render);
+})
